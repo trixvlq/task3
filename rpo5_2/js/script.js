0884afbccac21a491bdb9f05c942da1dbed12b7f
max <87362594+trixvlq@users.noreply.github.com>
2023-02-02 22:35:16 +0600

    Add files via upload

---
+++ rpo5_2/js/script.js
@@ -0,0 +1,24 @@
+function openModa(){
+    let layer = document.getElementById("layer");
+    let modal = document.getElementById("moda");
+    layer.style.display = "block"
+    modal.style.display = "block"
+}
+function openModal(){
+    let layer = document.getElementById("layer");
+    let modal = document.getElementById("modal");
+    layer.style.display = "block"
+    modal.style.display = "block"
+}
+function closeModal(){
+    let layer = document.getElementById("layer");
+    let modal = document.getElementById("modal");
+    layer.style.display = "none"
+    modal.style.display = "none"
+}
+function closeModa(){
+    let layer = document.getElementById("layer");
+    let modal = document.getElementById("moda");
+    layer.style.display = "none"
+    modal.style.display = "none"
+}
